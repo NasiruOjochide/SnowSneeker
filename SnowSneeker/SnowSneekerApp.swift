bebddb0556b03ac685b63ab73b84cbd89fc33801
Danjuma Nasiru <talk2danjumanas@gmail.com>
2023-03-27 08:21:00 +0100

    Initial Commit

---
+++ SnowSneeker/SnowSneekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSneekerApp.swift
+//  SnowSneeker
+//
+//  Created by Danjuma Nasiru on 27/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SnowSneekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
